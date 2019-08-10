728e535146dcee4ced77fdd485ddd57bf78399a3
Nicolas Fagotti <nicolasfagotti@gmail.com>
2019-08-10 20:02:02 -0300

    Add client logo to pdf

--- resources/views/pdf/bill.blade.php
+++ resources/views/pdf/bill.blade.php
@@ -4,6 +4,7 @@
 		<title>{!! $title !!}</title>
 </head>
 <body>
+  	{{ Html::image('img/logo.png', 'Estudio Michaud', array( 'width' => '100px', 'height' => '100px' )) }}
     <div style="margin: 20px 0 60px 0; text-align: right;">{!! $date !!}</div>
     <p>Recibí de {!! $clientName !!} la cantidad de pesos {!! $amountText !!}.</p>
     <div style="margin-top: 80px; text-align: right;"><b>{!! $amount !!}</b></div>
